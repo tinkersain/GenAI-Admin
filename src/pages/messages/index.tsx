a075fa0c80a8241e6e37402e887fe1bcf0c29787
tinkersain <tanishakar04@gmail.com>
2024-09-26 19:54:03 +0530

    compose messages section cmpleted

--- src/pages/messages/index.tsx
+++ src/pages/messages/index.tsx
@@ -1,7 +1,38 @@
-import { Accordion, AccordionItem, Avatar, Button } from "@nextui-org/react";
+import {
+  Accordion,
+  AccordionItem,
+  Avatar,
+  Button,
+  Select,
+  SelectItem,
+  Textarea,
+} from "@nextui-org/react";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from "@nextui-org/react";
+import { t } from "i18next";
+import toast from "react-hot-toast";
 import { IoIosSend } from "react-icons/io";
 
 export default function App() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const students = [
+    "John",
+    "Alice",
+    "Bob",
+    "Emma",
+    "Liam",
+    "Sophia",
+    "Ethan",
+    "Mia",
+    "Noah",
+    "Isabella",
+  ];
   const defaultContent =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
 
@@ -76,9 +107,66 @@ export default function App() {
             endContent={<IoIosSend style={{ fontSize: "30px" }} />}
             style={{ fontSize: "20px" }}
             variant="shadow"
+            onPress={onOpen}
           >
             Compose
           </Button>
+          <Modal
+            isOpen={isOpen}
+            onOpenChange={onOpenChange}
+            scrollBehavior="inside"
+          >
+            <ModalContent>
+              {(onClose) => (
+                <>
+                  <ModalHeader className="flex flex-col gap-1">
+                    Send a Message
+                  </ModalHeader>
+                  <ModalBody>
+                    <div className="w-full flex flex-col flex-wrap gap-4">
+                      <div>
+                        <Select
+                          key="success"
+                          color="success"
+                          label="Student Name"
+                          placeholder="Select a student"
+                          // defaultSelectedKeys={}
+                        >
+                          {students.map((student) => (
+                            <SelectItem key={student}>{student}</SelectItem>
+                          ))}
+                        </Select>
+                      </div>
+                      <div>
+                        <Textarea
+                          label="Description"
+                          placeholder="Enter the message"
+                          className="max-w-full"
+                        />
+                      </div>
+                    </div>
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button color="danger" variant="light" onPress={onClose}>
+                      Close
+                    </Button>
+                    <Button
+                      color="primary"
+                      onPress={() => {
+                        toast.success(t("Message Sent Successfully"), {
+                          position: "bottom-center",
+                        });
+                        onClose();
+                      }}
+                      endContent={<IoIosSend />}
+                    >
+                      Send
+                    </Button>
+                  </ModalFooter>
+                </>
+              )}
+            </ModalContent>
+          </Modal>
         </div>
       </div>
     </>
--- src/routes/_auth.messages.lazy.tsx
+++ src/routes/_auth.messages.lazy.tsx
@@ -1,14 +1,15 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
-import Messages from '../pages/messages'
-import { Helmet } from 'react-helmet'
-import { t } from 'i18next'
+import { createLazyFileRoute } from "@tanstack/react-router";
+import Messages from "../pages/messages";
+import { Helmet } from "react-helmet";
+import { t } from "i18next";
 
-export const Route = createLazyFileRoute('/_auth/messages')({
-    component: () =>
-        <>
-            <Messages />
-            <Helmet>
-                <title>{t("messages")}</title>
-            </Helmet>
-        </>
-})
+export const Route = createLazyFileRoute("/_auth/messages")({
+  component: () => (
+    <>
+      <Messages />
+      <Helmet>
+        <title>{t("messages")}</title>
+      </Helmet>
+    </>
+  ),
+});
